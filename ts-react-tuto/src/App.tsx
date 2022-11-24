8ac7185eab1e4d7d24b5840cd3132d21246bc389
unknown <je970311@gmail.com>
2022-11-25 01:49:39 +0900

    style-component

--- ts-react-tuto/src/App.tsx
+++ ts-react-tuto/src/App.tsx
@@ -1,19 +1,40 @@
-import './App.css';
-import Button from './button';
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import theme from "./theme";
 
-function App() {
-  const check = () => {
-    alert("이번 포켓몬은 뭘까요")
-  }
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  ${({ theme }) => theme.common.flexCenterColumn};
+`;
+
+const Title = styled.h1`
+  font-size: ${({ theme }) => theme.fontSizes.title};
+  color: ${({ theme }) => theme.colors.grey};
+`;
+
+const Subtitle = styled.h2`
+  font-size: ${({ theme }) => theme.fontSizes.subtitle};
+  color: ${({ theme }) => theme.colors.green};
+`;
+
+const Paragraph = styled.p`
+  font-size: ${({ theme }) => theme.fontSizes.Paragraph};
+  color: ${({ theme }) => theme.colors.blue};
+`;
+
+const App = () => {
   return (
     <div>
-      <div>
-        <Button width={100} onClick={check}>
-          <div>오박사의 포켓몬 기계</div>
-        </Button>
-      </div>
+      <ThemeProvider theme={theme}>
+        <Container>
+          <Title>Hello</Title>
+          <Subtitle>Welcome to styled-component's world</Subtitle>
+          <Paragraph>ThemeProvider에 대해서 배워볼까요?</Paragraph>
+        </Container>
+      </ThemeProvider>
     </div>
   );
-}
+};
 
-export default App;
+export default ThemeProviderPrac;
